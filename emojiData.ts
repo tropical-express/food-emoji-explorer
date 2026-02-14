e137cf0ca52254354f54a8a9c535804ad0bb7454
tropical-express <gnome2009123@gmail.com>
2026-02-14 17:57:03 -0600

    Add emojiData with Emoji interface and emoji list
    This file defines an Emoji interface and exports a list of emoji objects categorized by type, including fruits, prepared foods, sweets, drinks, and utensils.

---
+++ emojiData.ts
@@ -0,0 +1,45 @@
+export interface Emoji {
+  char: string;
+  name: string;
+}
+
+export const emojis: Emoji[] = [
+  // Fruits & Vegetables
+  {char:"🍏",name:"Green Apple"},{char:"🍎",name:"Red Apple"},{char:"🍐",name:"Pear"},
+  {char:"🍊",name:"Tangerine"},{char:"🍋",name:"Lemon"},{char:"🍌",name:"Banana"},
+  {char:"🍉",name:"Watermelon"},{char:"🍇",name:"Grapes"},{char:"🍓",name:"Strawberry"},
+  {char:"🫐",name:"Blueberries"},{char:"🥭",name:"Mango"},{char:"🥝",name:"Kiwi"},
+  {char:"🍒",name:"Cherries"},{char:"🍑",name:"Peach"},{char:"🥥",name:"Coconut"},
+  {char:"🥦",name:"Broccoli"},{char:"🥬",name:"Leafy Green"},{char:"🥒",name:"Cucumber"},
+  {char:"🥕",name:"Carrot"},{char:"🌽",name:"Ear of Corn"},{char:"🫑",name:"Bell Pepper"},
+  {char:"🧄",name:"Garlic"},{char:"🧅",name:"Onion"},{char:"🍄",name:"Mushroom"},
+  {char:"🥔",name:"Potato"},{char:"🍠",name:"Roasted Sweet Potato"},
+
+  // Prepared Foods
+  {char:"🥐",name:"Croissant"},{char:"🥯",name:"Bagel"},{char:"🥖",name:"Baguette Bread"},
+  {char:"🥨",name:"Pretzel"},{char:"🍞",name:"Bread"},{char:"🍚",name:"Cooked Rice"},
+  {char:"🍙",name:"Rice Ball"},{char:"🍘",name:"Rice Cracker"},{char:"🍜",name:"Steaming Bowl"},
+  {char:"🍝",name:"Spaghetti"},{char:"🍢",name:"Oden"},{char:"🍣",name:"Sushi"},
+  {char:"🍤",name:"Fried Shrimp"},{char:"🍥",name:"Fish Cake"},{char:"🥟",name:"Dumpling"},
+  {char:"🥠",name:"Fortune Cookie"},{char:"🥡",name:"Takeout Box"},
+
+  // Sweets
+  {char:"🍦",name:"Soft Ice Cream"},{char:"🍧",name:"Shaved Ice"},{char:"🍨",name:"Ice Cream"},
+  {char:"🍩",name:"Doughnut"},{char:"🍪",name:"Cookie"},{char:"🎂",name:"Birthday Cake"},
+  {char:"🍰",name:"Shortcake"},{char:"🧁",name:"Cupcake"},{char:"🍫",name:"Chocolate Bar"},
+  {char:"🍬",name:"Candy"},{char:"🍭",name:"Lollipop"},{char:"🍮",name:"Custard"},
+  {char:"🍯",name:"Honey Pot"},
+
+  // Drinks
+  {char:"🥛",name:"Glass of Milk"},{char:"🍼",name:"Baby Bottle"},{char:"☕",name:"Hot Beverage"},
+  {char:"🫖",name:"Teapot"},{char:"🍵",name:"Tea Cup"},{char:"🥤",name:"Cup with Straw"},
+  {char:"🧃",name:"Beverage Box"},{char:"🍶",name:"Sake"},{char:"🍺",name:"Beer Mug"},
+  {char:"🍻",name:"Clinking Beer Mugs"},{char:"🥂",name:"Clinking Glasses"},{char:"🍷",name:"Wine Glass"},
+  {char:"🥃",name:"Tumbler Glass"},{char:"🍸",name:"Cocktail Glass"},{char:"🍹",name:"Tropical Drink"},
+  {char:"🍾",name:"Bottle with Popping Cork"},{char:"🧉",name:"Mate"},{char:"🧊",name:"Ice"},
+
+  // Utensils
+  {char:"🍽️",name:"Fork and Knife with Plate"},{char:"🍴",name:"Fork and Knife"},
+  {char:"🥄",name:"Spoon"},{char:"🔪",name:"Kitchen Knife"},{char:"🥣",name:"Bowl with Spoon"},
+  {char:"🫙",name:"Jar"}
+];
